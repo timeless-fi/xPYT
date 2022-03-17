e8540dd4bfcf4999600af81dbf3ceb3e92c30520
ZeframLou <zeframlou@gmail.com>
2022-03-17 15:47:53 -0700

    feat: update factory

--- src/xPYTFactory.sol
+++ src/xPYTFactory.sol
@@ -3,9 +3,10 @@ pragma solidity ^0.8.4;
 
 import {ERC20} from "solmate/tokens/ERC20.sol";
 
-import {TLPool} from "timeless-amm/TLPool.sol";
+import {IQuoter} from "v3-periphery/interfaces/IQuoter.sol";
 
 import {xPYT} from "./xPYT.sol";
+import {UniswapV3xPYT} from "./uniswap-v3/UniswapV3xPYT.sol";
 
 /// @title xPYTFactory
 /// @author zefram.eth
@@ -13,25 +14,27 @@ import {xPYT} from "./xPYT.sol";
 contract xPYTFactory {
     event DeployXPYT(ERC20 indexed asset_, xPYT deployed);
 
-    function deployXPYT(
+    function deployUniswapV3xPYT(
         ERC20 asset_,
         string memory name_,
         string memory symbol_,
-        TLPool ammPool_,
         uint256 pounderRewardMultiplier_,
-        uint16 twapLookbackDistance_,
-        uint256 twapMinLookbackTime_,
-        uint256 minOutputMultiplier_
+        uint256 minOutputMultiplier_,
+        address uniswapV3Factory_,
+        IQuoter uniswapV3Quoter_,
+        uint24 uniswapV3PoolFee_,
+        uint32 uniswapV3TwapSecondsAgo_
     ) external returns (xPYT deployed) {
-        deployed = new xPYT(
+        deployed = new UniswapV3xPYT(
             asset_,
             name_,
             symbol_,
-            ammPool_,
             pounderRewardMultiplier_,
-            twapLookbackDistance_,
-            twapMinLookbackTime_,
-            minOutputMultiplier_
+            minOutputMultiplier_,
+            uniswapV3Factory_,
+            uniswapV3Quoter_,
+            uniswapV3PoolFee_,
+            uniswapV3TwapSecondsAgo_
         );
 
         emit DeployXPYT(asset_, deployed);
