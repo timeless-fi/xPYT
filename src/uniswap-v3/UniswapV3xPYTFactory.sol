6ef5449cbc4de0ec1fb058fb7cca5c11a11dff91
ZeframLou <zeframlou@gmail.com>
2022-08-10 21:34:36 -0700

    feat: add pool deployment to UniswapV3xPYTFactory

--- src/uniswap-v3/UniswapV3xPYTFactory.sol
+++ src/uniswap-v3/UniswapV3xPYTFactory.sol
@@ -3,7 +3,11 @@ pragma solidity ^0.8.4;
 
 import {ERC20} from "solmate/tokens/ERC20.sol";
 
+import {NegativeYieldToken} from "timeless/NegativeYieldToken.sol";
 import {PerpetualYieldToken} from "timeless/PerpetualYieldToken.sol";
+
+import {IUniswapV3Pool} from "v3-core/interfaces/IUniswapV3Pool.sol";
+import {IUniswapV3Factory} from "v3-core/interfaces/IUniswapV3Factory.sol";
 
 import {IQuoter} from "v3-periphery/interfaces/IQuoter.sol";
 
@@ -16,34 +20,53 @@ import {UniswapV3xPYT} from "./UniswapV3xPYT.sol";
 contract UniswapV3xPYTFactory {
     event DeployXPYT(PerpetualYieldToken indexed pyt, xPYT deployed);
 
-    address public immutable uniswapV3Factory;
+    IUniswapV3Factory public immutable uniswapV3Factory;
     IQuoter public immutable uniswapV3Quoter;
 
-    constructor(address uniswapV3Factory_, IQuoter uniswapV3Quoter_) {
+    constructor(IUniswapV3Factory uniswapV3Factory_, IQuoter uniswapV3Quoter_) {
         uniswapV3Factory = uniswapV3Factory_;
         uniswapV3Quoter = uniswapV3Quoter_;
     }
 
     function deployUniswapV3xPYT(
         PerpetualYieldToken pyt,
+        NegativeYieldToken nyt,
         string memory name_,
         string memory symbol_,
         uint256 pounderRewardMultiplier_,
         uint256 minOutputMultiplier_,
         uint24 uniswapV3PoolFee_,
-        uint32 uniswapV3TwapSecondsAgo_
-    ) external returns (xPYT deployed) {
+        uint32 uniswapV3TwapSecondsAgo_,
+        uint160 sqrtPriceX96,
+        uint16 observationCardinalityNext
+    ) external returns (xPYT deployed, IUniswapV3Pool pool) {
+        // deploy xPYT
         deployed = new UniswapV3xPYT(
             pyt,
             name_,
             symbol_,
             pounderRewardMultiplier_,
             minOutputMultiplier_,
-            uniswapV3Factory,
+            address(uniswapV3Factory),
             uniswapV3Quoter,
             uniswapV3PoolFee_,
             uniswapV3TwapSecondsAgo_
         );
+
+        // deploy Uniswap pool
+        pool = IUniswapV3Pool(
+            uniswapV3Factory.createPool(
+                address(nyt),
+                address(deployed),
+                uniswapV3PoolFee_
+            )
+        );
+
+        // initialize pool price
+        pool.initialize(sqrtPriceX96);
+
+        // update Uniswap pool observation
+        pool.increaseObservationCardinalityNext(observationCardinalityNext);
 
         emit DeployXPYT(pyt, deployed);
     }
